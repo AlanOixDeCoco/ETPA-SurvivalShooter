3af9d7f898a7d86e51b68d874278f88a2b428d40
AlanOixDeCoco <49500226+AlanOixDeCoco@users.noreply.github.com>
2023-11-05 22:01:18 +0100

    Update StateMachine.cs

--- ETPA Survival Shooter/Assets/Scripts/StateMachine.cs
+++ ETPA Survival Shooter/Assets/Scripts/StateMachine.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class StateMachine : MonoBehaviour
+public interface IState
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Update();
+    public void ExitState();
+    public void EnterState();
+}
+
+public class StateMachine
+{
+    public IState _currentState { get; private set; }
+
+    public void UpdateState()
     {
-        
+        _currentState.Update();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SwitchState(IState nextState)
     {
-        
+        _currentState.ExitState();
+  
+        _currentState = nextState;
+
+        _currentState.EnterState();
     }
 }
